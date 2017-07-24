dbf9d076a9162eaa51486d74a8c3da58127f53b0
Jordi Puigdellívol <jordi@gloobus.net>
2017-07-24 11:33:30 +0200

    CreateWith uses self

--- src/Models/PurchaseOrder.php
+++ src/Models/PurchaseOrder.php
@@ -21,7 +21,7 @@ class PurchaseOrder extends Model {
     public static function createWith($vendor_id, $items, $status = PurchaseOrderContent::STATUS_PENDING){
         if( ! count($items) ) return null;
 
-        $order = tap(PurchaseOrder::create( compact('vendor_id', 'status') ), function ($order) use ($items) {
+        $order = tap(self::create( compact('vendor_id', 'status') ), function ($order) use ($items) {
             return $order->contents()->createMany(collect($items)->map(function ($item) use ($order) {
                 return (new PurchaseOrderContent([
                     'status'         => $order->status,
