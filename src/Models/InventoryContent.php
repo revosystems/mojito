fca446e24aba80587930b7b88ff1a544a60ea758
paubenetprat <pau@revo.works>
2018-09-20 09:32:02 +0200

    add sale action type

--- src/Models/InventoryContent.php
+++ src/Models/InventoryContent.php
@@ -104,8 +104,9 @@ class InventoryContent extends Model
     protected function consumedQuantityWithAdd($lastInventoryClosedAt)
     {
         return $this->stockMovementsQuery($lastInventoryClosedAt)
-            ->where("action", $this->warehouseClass::ACTION_ADD)
-            ->where("to_warehouse_id", $this->inventory->warehouse_id)
+            ->where(function ($query) {
+                $query->where("action", $this->warehouseClass::ACTION_ADD)->orWhere('action', $this->warehouseClass::ACTION_SALE);
+            })->where("to_warehouse_id", $this->inventory->warehouse_id)
             ->where("quantity", "<", 0)->sum('quantity');
     }
 
