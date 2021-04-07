4e9d49787c2ccc29aa7f8eb5b6a9985446dd67f6
adriamartin <adria.martin@revo.works>
2021-04-07 15:57:46 +0200

    Adds missing translation

--- src/Exceptions/UnitsNotCompatibleException.php
+++ src/Exceptions/UnitsNotCompatibleException.php
@@ -8,6 +8,6 @@ class UnitsNotCompatibleException extends Exception
 {
     public function __construct()
     {
-        parent::__construct("Units not compatible");
+        parent::__construct(__("admin.unitsNotCompatible"));
     }
 }
